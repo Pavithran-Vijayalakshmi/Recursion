4d3c683558beab4f8e26f01b97f77c428bd1d84b
Pavithran-Vijayalakshmi <pavithranvijayalakshmi@gmail.com>
2025-05-09 14:15:11 +0530

    coded subsequence.py

---
+++ subsequence.py
@@ -0,0 +1,14 @@
+def subs(ind,given,arr):
+    if ind == len(given):
+        for i in arr:
+            print(i,end = " ")
+        print()
+        return
+    arr.append(given[ind])
+    subs(ind+1,given,arr)
+    arr.pop()
+    subs(ind+1,given,arr)
+
+arr = [3,1,2]
+subs(0,arr,[])   
+    
